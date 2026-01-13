601182eedeab9ef36f3112fd2a60043e931200ff
mayoina29 <ryals29@naver.com>
2026-01-13 21:26:11 +0900

    Initial commit: ST7789V2 Linux Framebuffer Driver (Step 8)

--- myspi.c
+++ myspi.c
@@ -4,17 +4,21 @@
 #include <linux/spi/spi.h>
 #include <linux/gpio/consumer.h>
 #include <linux/delay.h>
-#include <linux/fs.h>
-#include <linux/miscdevice.h>
-#include <linux/uaccess.h>
+#include <linux/fb.h>
+#include <linux/vmalloc.h>
+#include <linux/slab.h>
 
+#define DRV_WIDTH 240 
+#define DRV_HEIGHT 280
 
 struct myspi_dev {
     struct spi_device *spi;
     struct gpio_desc *reset_gpio;
     struct gpio_desc *dc_gpio;
-    struct miscdevice miscdev;
-    char kbuf[32];
+    struct fb_info *info;
+    void *vram;
+    u32 vram_size;
+    u32 pseudo_palette[16];
 };
 
 static void write_command(struct myspi_dev *dev, u8 cmd){
@@ -27,6 +31,34 @@ static void write_data(struct myspi_dev *dev, u8 data){
     gpiod_set_value(dev->dc_gpio, 1);
 
     spi_write(dev->spi, &data, 1);
+}
+
+static void st7789_set_window(struct myspi_dev *dev, u16 x, u16 y, u16 w, u16 h){
+
+        u16 x_end = x + w -1;
+        u16 y_end = y + h -1;
+        
+        
+        int y_off =20;
+        y += y_off;
+        y_end += y_off;
+
+        // CASET (0x2A) x 좌표 (열) 설정
+        write_command(dev, 0x2A);
+        write_data(dev, x >> 8);
+        write_data(dev, x & 0xFF);
+        write_data(dev, x_end >> 8);
+        write_data(dev, x_end & 0xFF);
+        
+        // RASET (0x2B: y좌표 (행) 설정
+        write_command(dev, 0x2B);
+        write_data(dev, y >> 8);
+        write_data(dev, y & 0xFF);
+        write_data(dev, y_end >> 8);
+        write_data(dev, y_end & 0xFF);
+
+        // RAMWR (0x2C) 메모리 쓰기 시작 알림 
+        write_command(dev, 0x2C);
 }
 
 static void st7789_init(struct myspi_dev *dev){
@@ -49,6 +81,17 @@ static void st7789_init(struct myspi_dev *dev){
         write_command(dev, 0x36);
         write_data(dev, 0x00); // 0x00 = 정방향 (필요시 0x60, 0xC0 등으로 변경)
 
+        /*//1.69인치 240x280 오프셋 보정 
+        write_command(dev, 0x2A); //CASET 
+        write_data(dev, 0); write_data(dev,0);
+        write_data(dev, 0); write_data(dev,239);
+
+        write_command(dev, 0x2B); //RASET (offset +20) 
+        write_data(dev, 0); write_data(dev,20);
+        write_data(dev, 300>>8); write_data(dev,300 & 0xFF);
+       */
+       st7789_set_window(dev, 0,0, DRV_WIDTH, DRV_HEIGHT);
+
         // INVON (0x21): 색상 반전 켜기 (IPS 패널 특성상 반전이 필요할 수 있음
         // 화면이 색반전되면 이걸 주석처리하거나 INVOFF(0x20) 을 사용하자 
         write_command(dev, 0x21);
@@ -62,32 +105,18 @@ static void st7789_init(struct myspi_dev *dev){
         pr_info("ST7789V2: Initialization done. Display should be On \n");
 }
 
-static void st7789_set_window(struct myspi_dev *dev, u16 x_start, u16 y_start, u16 x_end, u16 y_end){
+static struct fb_ops myspi_fb_ops = {
+        .owner = THIS_MODULE,
+        .fb_read = fb_sys_read,
+        .fb_write = fb_sys_write,
+        .fb_fillrect = sys_fillrect,
+        .fb_copyarea = sys_copyarea,
+        .fb_imageblit = sys_imageblit,
+        .fb_mmap = fb_deferred_io_mmap,
+};
 
-        int y_offset =20;
 
-        y_start += y_offset;
-        y_end += y_offset;
-
-        // CASET (0x2A) x 좌표 (열) 설정
-        write_command(dev, 0x2A);
-        write_data(dev, x_start >> 8);
-        write_data(dev, x_start & 0xFF);
-        write_data(dev, x_end >> 8);
-        write_data(dev, x_end & 0xFF);
-        
-        // RASET (0x2B: y좌표 (행) 설정
-        write_command(dev, 0x2B);
-        write_data(dev, y_start >> 8);
-        write_data(dev, y_start & 0xFF);
-        write_data(dev, y_end >> 8);
-        write_data(dev, y_end & 0xFF);
-
-        // RAMWR (0x2C) 메모리 쓰기 시작 알림 
-        write_command(dev, 0x2C);
-}
-
-static void st7789_fill(struct myspi_dev *dev, u16 color){
+/*static void st7789_fill(struct myspi_dev *dev, u16 color){
     int i;
 
     int width = 240;
@@ -151,16 +180,56 @@ static const struct file_operations myspi_fops = {
        .open = myspi_open,
        .write = myspi_write,
 };
+*/
 
-static int myspi_prove(struct spi_device *spi){
+static void myspi_fb_deferred_io(struct fb_info *info, struct list_head *pagelist){
+        
+        struct myspi_dev *dev = info->par;
+        u16 *vram16 = (u16 *)dev->vram;
+
+        u8 txbuf[480];
+        int x,y;
+        
+        pr_info("ST7789V2: Drawing screen...\n");
+        st7789_set_window(dev, 0,0, DRV_WIDTH, DRV_HEIGHT);
+
+        gpiod_set_value(dev->dc_gpio, 1);
+
+        for(y= 0; y <DRV_HEIGHT; y++){
+            for (x=0; x<DRV_WIDTH; x++){
+                u16 pixel = vram16[y * DRV_WIDTH + x];
+                txbuf[x*2] = pixel >> 8;
+                txbuf[x*2+1] = pixel & 0xFF;
+            }
+            spi_write(dev->spi, txbuf, DRV_WIDTH *2);
+        }
+}
+
+static struct fb_deferred_io myspi_defio ={
+        .delay = HZ / 20,
+        .deferred_io = myspi_fb_deferred_io,
+};
+
+
+
+
+
+static int myspi_probe(struct spi_device *spi){
         struct myspi_dev *dev;
+        struct fb_info *info;
+        int ret;
+        u32 vram_size;
+        u8 *vram;
 
-        pr_info("ST7789V2_169 Driver:  Probing device... Start GPIO Setup!\n");
+        pr_info("ST7789V2_169 :  Probing FB setup...\n");
 
-        dev = devm_kzalloc(&spi->dev, sizeof(*dev), GFP_KERNEL);
-        if(!dev) return -ENOMEM;
+        //fb_info 구조체 할당 
+        info = framebuffer_alloc(sizeof(struct myspi_dev), &spi->dev);
+        if(!info) return -ENOMEM;
 
+        dev = info->par;
         dev->spi = spi;
+        dev->info  = info;
         spi_set_drvdata(spi, dev);
 
         dev->reset_gpio = devm_gpiod_get(&spi->dev, "reset", GPIOD_OUT_HIGH);
@@ -189,30 +258,72 @@ static int myspi_prove(struct spi_device *spi){
         //pr_info("ST7789V2: Initialization sequence finished. LCD is ready!\n");
 
         st7789_init(dev);
+
+        //가상 vram 할당 
+        //크기 가로 x 세로 x 2바이트 16bit 
+        vram_size = DRV_WIDTH * DRV_HEIGHT * 2;
+        vram = vmalloc(vram_size);
+        if(!vram) return -ENOMEM;
+        memset(vram, 0 , vram_size);
+
+        dev->vram = vram;
+        dev->vram_size = vram_size;
+
+        strcpy(info->fix.id, "ST7789");
+
+        //fb_info 설정 (모니터 스펙작성)
+        info->fbops = &myspi_fb_ops;
+        info->screen_base = (char __iomem *)vram;//vram 주소 등록
+        info->screen_size = vram_size;
+
+        //고정 정보
+        info->fix.type = FB_TYPE_PACKED_PIXELS;
+        info->fix.visual = FB_VISUAL_TRUECOLOR;
+        info->fix.accel = FB_ACCEL_NONE;
+        info->fix.line_length = DRV_WIDTH*2;
+        info->fix.smem_start = (unsigned long)vram;
+        info->fix.smem_len = vram_size;
+
+        // 변동 정보 - 해상도 색상깊이
+        info->var.xres = DRV_WIDTH;
+        info->var.yres = DRV_HEIGHT;
+        info->var.xres_virtual = DRV_WIDTH;
+        info->var.yres_virtual = DRV_HEIGHT;
+        info->var.bits_per_pixel = 16;
+
+        // RGB565 포맷 정의 (R 5bit G 6bit B 5bit )
+        info->var.red.offset = 11; info->var.red.length = 5;
+        info->var.green.offset = 5; info->var.green.length = 6;
+        info->var.blue.offset = 0; info->var.blue.length = 5;
+        info->var.activate = FB_ACTIVATE_NOW;
+
+        info->pseudo_palette = dev->pseudo_palette;
         
-        //pr_info("ST7789V2: Paninting Screen with #FE5069 (0xFA8D)...\n");
+        info->fbdefio = &myspi_defio;
+        fb_deferred_io_init(info);
 
-        //st7789_fill(dev, 0xFA8D);
-
-        //pr_info("ST7789V2: Painting Done\n");
-
-        dev->miscdev.minor = MISC_DYNAMIC_MINOR;
-        dev->miscdev.name = "st7789";
-        dev->miscdev.fops = &myspi_fops;
-        dev->miscdev.parent = &spi->dev;
-
-        if (misc_register(&dev->miscdev)){
-            pr_err("ST7789: Failed to register misc device\n");
-            return -EBUSY;
+        //프레임 버퍼 등록
+        ret = register_framebuffer(info);
+        if(ret < 0){
+            pr_err("ST7789V2: Failed to register framebuffer\n");
+            return ret;
         }
-        pr_info("ST7789V2: Created /dev/st7789 successfully!\n");
+
+        pr_info("ST7789V2: framebuffer /dev/fbX registered!\n");
         return 0;
 }
 
 static void myspi_remove(struct spi_device *spi){
     struct myspi_dev *dev = spi_get_drvdata(spi);
-    misc_deregister(&dev->miscdev);
+    //misc_deregister(&dev->miscdev);
 
+    //등록 해제
+    if(dev->info){
+        fb_deferred_io_cleanup(dev->info);
+        unregister_framebuffer(dev->info);
+        framebuffer_release(dev->info);
+    }
+    if(dev->vram) vfree(dev->vram);
 	pr_info("ST7789V2_169: Removing driver... Goodbye!\n");
 }
 
@@ -228,7 +339,7 @@ static struct spi_driver myspi_driver = {
 		.owner = THIS_MODULE,
 		.of_match_table = myspi_dt_ids,
 	},
-	.probe = myspi_prove,
+	.probe = myspi_probe,
 	.remove = myspi_remove,
 };
 module_spi_driver(myspi_driver);
